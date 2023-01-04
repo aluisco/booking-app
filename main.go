482fdf000254567d1ef09a3ed650294b9c8ca640
Aluisco Miguel Ricardo Mastrapa <aluisco2005@gmail.com>
2023-01-04 13:22:42 -0500

    Some initial definitions

--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v Application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets, and still avaliable %v\n", conferenceTickets, remainingTickets)
+	fmt.Printf("Get your tickets here\n")
+
 }
